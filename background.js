ee9fdd3d9e14ddea9cc0df3898a512f84974fe16
Bailey Alger <balger455@gmail.com>
2023-04-30 23:03:39 -0400

    add store userID locally

--- background.js
+++ background.js
@@ -2,6 +2,10 @@ import { CLIENT_SECRET } from "./config.js";
 
 const tokenPromise = fetchOAuth();
 
+chrome.storage.local.set({ userID: userID }, function() {
+    console.log('userID has been saved to local storage');
+  });
+
 async function fetchOAuth() {
     let response = await fetch("https://id.twitch.tv/oauth2/token", {
         method: "POST",
@@ -15,6 +19,13 @@ async function fetchOAuth() {
 };
 
 async function fetchUserID() {
+    // Check if userID is stored locally
+    const userID = await getStoredUserID();
+    if (userID) {
+        return userID;
+    }
+
+    // Fetch userID if not stored locally
     const authToken = await tokenPromise;
     let response = await fetch("https://api.twitch.tv/helix/users?login=tiltzer", {
         headers: {
@@ -23,9 +34,28 @@ async function fetchUserID() {
         }
     });
     let data = await response.json();
-    return data.data[0].id;
+    const fetchedUserID = data.data[0].id;
     // 1st data = promise, 2nd data = array of responses. Likely because this fetch can return multiple responses? Perhaps?
+    await setStoredUserID(fetchedUserID);
+    return fetchedUserID;
 };
+
+function getStoredUserID() {
+    return new Promise((resolve) => {
+      chrome.storage.local.get(["userID"], (result) => {
+        const userID = result.userID;
+        resolve(userID);
+      });
+    });
+  }
+  
+  function setStoredUserID(userID) {
+    return new Promise((resolve) => {
+      chrome.storage.local.set({ userID: userID }, () => {
+        resolve();
+      });
+    });
+  }
 
 // const userIDPromise = fetchUserID();
 
@@ -42,12 +72,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 (async function () {
     console.log("message recieved: " + request);
     if (request === "fetchData") {
+        const rUserID = await fetchUserID();
         const token = await tokenPromise;
-        const userID = await fetchUserID();
         sendResponse({
             success: true,
             token: token,
-            userID: userID
+            userID: rUserID
         });
     }
 })();
