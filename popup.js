18efa0a1ed8e0cd8e95a5430c2e230c6c2978ae3
Bailey Alger <balger455@gmail.com>
2023-05-03 23:14:20 -0400

    add html access to twitch follow list

--- popup.js
+++ popup.js
@@ -2,7 +2,16 @@ chrome.runtime.sendMessage("fetchData", async function(response) {
     if (response && response.followList) {
         console.log("response successful");
         console.log(response.followList);
+        const followList = response.followList;
+        let twitchList = document.getElementById("twitchList");
+        followList.forEach((item) => {
+            let li = document.createElement("li");
+            li.innerText = item;
+            twitchList.appendChild(li);
+        });
+        
     } else {
         console.error("Error retrieving data from background script");
     }
 });
+
