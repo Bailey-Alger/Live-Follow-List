bacecd207bef777652ef9d9c6d0311ca8588175b
Bailey Alger <balger455@gmail.com>
2024-01-28 23:25:14 -0500

    add react towtch login button component

---
+++ src/components/TwitchLogin.jsx
@@ -0,0 +1,29 @@
+function TwitchLogin() {
+    return (
+        <>
+            <button
+                onClick={() => {
+                    chrome.identity.launchWebAuthFlow(
+                        {
+                            url: `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=pa669by8xti1oag6giphneaeykt6ln&redirect_uri=https%3A%2F%2F${extID}.chromiumapp.org&scope=user%3Aread%3Afollows`,
+                            interactive: true,
+                        },
+                        function (redirect_url) {
+                            console.log(redirect_url);
+                            sendOAuthURL(redirect_url);
+                        }
+                    );
+                    console.log("url sent to background script");
+                    loginClicked = true;
+                    setTimeout(() => {
+                        fetchTwitchData();
+                    }, 2000);
+                }}
+            >
+                Login with Twitch
+            </button>
+        </>
+    );
+}
+
+export default TwitchLogin;
