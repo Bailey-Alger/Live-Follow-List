2cb4d766db6956d7f4aea5a7ad0c34880d9c499b
Bailey Alger <balger455@gmail.com>
2024-01-28 23:25:56 -0500

    add react twitch component

---
+++ src/components/Twitch.jsx
@@ -0,0 +1,13 @@
+import TwitchFollowList from "./TwitchFollowList";
+import TwitchLogin from "./TwitchLogin";
+
+function Twitch() {
+    return (
+        <>
+            <TwitchLogin />
+            <TwitchFollowList />
+        </>
+    );
+}
+
+export default Twitch;
