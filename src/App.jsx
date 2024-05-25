4b7e401154871072f667762ac8a9a0d1db029b98
Bailey Alger <balger455@gmail.com>
2024-05-25 02:58:47 -0400

    add content and routing to twitch(home) and settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { Link, Route, Routes } from "react-router-dom";
 import Twitch from "./components/Twitch";
+import { Settings } from "./components/Settings";
 
 function App() {
     return (
         <>
-            <Twitch />
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/settings">Settings</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Twitch />} />
+                <Route path="/settings" element={<Settings />} />
+            </Routes>
         </>
     );
 }
